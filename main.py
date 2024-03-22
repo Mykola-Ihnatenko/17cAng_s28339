cccbab10783dc37a6d32964de8cb552648b9bf11
Mykola Ihnatenko <s28339@pjwstk.edu.pl>
2024-03-22 13:24:15 +0100

    Task 6

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-import math
-
-
-class SquareGenerator:
-    def generate_squares(start, end):
-        if end <= start:
-            raise ValueError("End of the range must be greater than the start.")
-
-        squares = [x ** 2 for x in range(start, end)]
-        return squares
-
-    def calculate_square_roots(self, numbers):
-        sq = [math.sqrt(num) for num in numbers]
-        return sq
-
+from square_generator import SquareGenerator
 
 a = SquareGenerator()
 res = a.generate_squares(1, 11)
---
+++ square_generator.py
@@ -0,0 +1,11 @@
+class SquareGenerator:
+    def generate_squares(start, end):
+        if end <= start:
+            raise ValueError("End of the range must be greater than the start.")
+
+        squares = [x ** 2 for x in range(start, end)]
+        return squares
+
+    def calculate_square_roots(self, numbers):
+        sq = [math.sqrt(num) for num in numbers]
+        return sq
