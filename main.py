7e93e7db38b14d262b054ac6f814e66858fef8e7
Mykola Ihnatenko <s28339@pjwstk.edu.pl>
2024-03-22 13:19:18 +0100

    Task 4

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+import math
+
+
 class SquareGenerator:
     def generate_squares(start, end):
-        squares = [x**2 for x in range(start, end)]
+        squares = [x ** 2 for x in range(start, end)]
         return squares
+
+    def calculate_square_roots(self, numbers):
+        sq = [math.sqrt(num) for num in numbers]
+        return sq
 
 
 a = SquareGenerator()
-result = a.generate_squares(1, 11)
-print(result)
+res = a.generate_squares(1, 11)
+sq = a.calculate_square_roots(res)
+print(res)
+print(sq)
