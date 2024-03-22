430de78112ada705cabf250753e1e49c1a6918b5
Mykola Ihnatenko <s28339@pjwstk.edu.pl>
2024-03-22 13:34:08 +0100

    Task 9

--- square_generator/square_generator.py
+++ square_generator/square_generator.py
@@ -1,9 +1,6 @@
 import math
 class SquareGenerator:
     def generate_squares(self, start, end):
-        if end <= start:
-            raise ValueError("End of the range must be greater than the start.")
-
         squares = [x ** 2 for x in range(start, end)]
         return squares
 
