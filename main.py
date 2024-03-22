8e279ec0eac414f6205056611549571b479c971b
Mykola Ihnatenko <s28339@pjwstk.edu.pl>
2024-03-22 13:14:03 +0100

    Task 2

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-squares = [x**2 for x in range(1, 11)]
-print(squares)
+
+
+def generate_squares(start, end):
+    squares = [x**2 for x in range(start, end)]
+    return squares
+
+result = generate_squares(1, 11)
+print(result)
